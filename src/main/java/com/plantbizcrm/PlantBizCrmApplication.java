e6e94b19a5b1c31513ee7f0004459ca5714a8723
ChetanSawant <chetansawant.dev@gmail.com>
2024-07-14 16:04:11 +0530

    Initial Commit for CRM Module

---
+++ src/main/java/com/plantbizcrm/PlantBizCrmApplication.java
@@ -0,0 +1,13 @@
+package com.plantbizcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantBizCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantBizCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantbizcrm/PlantBizCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantbizcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantBizCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
